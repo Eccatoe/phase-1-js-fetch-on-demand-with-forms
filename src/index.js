cf559364f42b91123cb488c3023a3581b855640b
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-23 18:48:13 -0500

    commit tree 3365b6bd8dcc8a7c527ac94de9f296c3eec91be9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const init = () => {
-  
-}
+init()
 
-document.addEventListener('DOMContentLoaded', init);
+function init(){
+    const form=document.querySelector('form')
+    form.addEventListener('submit', (e)=>{           
+        e.preventDefault()
+        const input=document.querySelector('input#searchByID')
+        fetch (`http://localhost:3000/movies/${input.value}`)
+        .then (response=>response.json())
+        .then (movieArray=>{
+            const title=document.querySelector('section#movieDetails h4')
+            const summary=document.querySelector('section#movieDetails p')
+    
+            title.textContent=movieArray.title
+            summary.textContent=movieArray.summary
+        
+    })
+})
+}
